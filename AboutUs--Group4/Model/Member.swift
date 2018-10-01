99c6d908011adf77e967f08bc337ed95347ee9dc
John Tate <johntate@Johns-MacBook-Pro.local>
2018-10-01 17:55:32 -0600

    troubleshooting

---
+++ AboutUs--Group4/Model/Member.swift
@@ -0,0 +1,9 @@
+//
+//  Member.swift
+//  AboutUs--Group4
+//
+//  Created by John Tate on 10/1/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+import Foundation
---
+++ AboutUs--Group4/View/CardCellCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardCellCollectionViewCell.swift
+//  AboutUs--Group4
+//
+//  Created by John Tate on 10/1/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+import UIKit
+
+class CardCellCollectionViewCell: UICollectionViewCell {
+    
+}
